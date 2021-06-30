f86d76181ca4e2031d20858d557f59c29b090b89
Haley Parish <heparish@ucdavis.edu>
2021-06-30 14:38:04 -0700

    added models

---
+++ models/comment.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {  
+}
+
+Comment.init (
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true,
+          },
+        body: {
+            type: DataTypes.STRING,
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id',
+            }
+      },
+      sequelize,
+      timestamps: true,
+    }
+);
+
+module.exports = Comment;
+
