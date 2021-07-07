22a79335d78b6166ecadca65f5f5d52c38207fbd
Haley Parish <heparish@ucdavis.edu>
2021-07-06 23:57:30 -0700

    crying

--- server.js
+++ server.js
@@ -3,10 +3,28 @@ const express = require("express")
 const expresSession= require("express-session")
 const exphbs =require("express-handlebars")
 const sequelize= require("./config/connection")
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
 
 const app= express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({ helpers });
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 // Parse request body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -15,7 +33,7 @@ app.use(express.static("public"));
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
-
+app.use(routes);
 
 sequelize.sync().then(()=>{
 app.listen(PORT, function() {
