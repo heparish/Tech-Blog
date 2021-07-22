7d6af782eccf16660272575c9dc831a4282e67a0
Haley Parish <heparish@ucdavis.edu>
2021-07-22 14:12:42 -0700

    name change

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
-const Post = require('./Post');
-const Comment = require('./Comment');
+const Post = require('./Blog');
+const Comment = require('./Opinions');
 
 User.hasMany(Post, {
   foreignKey: 'user_id',
