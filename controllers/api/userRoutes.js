43fb7698192ad78201075f0a90808e7ed3a67017
Haley Parish <heparish@ucdavis.edu>
2021-07-22 14:17:32 -0700

    heroku stuff

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,6 @@
-const router = require('express').Router();
+const express = require('express');
+const router = express.Router();
+
 const { User, Post, Comment } = require('../../models');
 
 // GET all users test
